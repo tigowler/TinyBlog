7915da2c6647fabee0df92bdb57bb640ed2e89cc
tigowler <aisapple1030@naver.com>
2022-02-01 13:17:38 +0900

    link guest book with db

--- src/main/java/com/neoAA/TinyBoard/controller/GuestBookController.java
+++ src/main/java/com/neoAA/TinyBoard/controller/GuestBookController.java
@@ -1,16 +1,26 @@
 package com.neoAA.TinyBoard.controller;
 
+import com.neoAA.TinyBoard.model.GuestBook;
+import com.neoAA.TinyBoard.repository.GuestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/guest")
 public class GuestBookController {
 
+    @Autowired
+    private GuestRepository guestRepository;
+
     @GetMapping
     public String guestBook(Model model){
+        List<GuestBook> mentions = guestRepository.findAll();
+        model.addAttribute("mentions", mentions);
         return "guest";
     }
 }
