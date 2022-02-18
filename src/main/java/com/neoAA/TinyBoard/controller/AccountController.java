5b0bb1014f4aa35d64b7946cee984d46ea8f8aac
tigowler <aisapple1030@naver.com>
2022-02-18 14:30:56 +0900

    user join validation

--- src/main/java/com/neoAA/TinyBoard/controller/AccountController.java
+++ src/main/java/com/neoAA/TinyBoard/controller/AccountController.java
@@ -4,9 +4,14 @@ import com.neoAA.TinyBoard.Service.UserService;
 import com.neoAA.TinyBoard.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/account")
@@ -21,12 +26,16 @@ public class AccountController {
     }
 
     @GetMapping("register")
-    public String register(){
+    public String register(Model model){
+        model.addAttribute("user", new User());
         return "account/register";
     }
 
     @PostMapping("register")
-    public String register(User user){
+    public String register(@Valid User user, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "account/register";
+        }
         userService.save(user);
         return "redirect:/";
     }
--- src/main/java/com/neoAA/TinyBoard/model/User.java
+++ src/main/java/com/neoAA/TinyBoard/model/User.java
@@ -3,6 +3,7 @@ package com.neoAA.TinyBoard.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,7 +14,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Size(min = 2, max = 8, message = "❌ User name must be between {min} and {max} characters long :(")
     private String username;
+
+    @Size(min = 4, max = 11, message = "❌ Password must be between {min} and {max} characters long :(")
     private String password;
     private Boolean enabled;
 
