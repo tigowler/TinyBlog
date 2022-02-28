3953bdd0324e8156b728f865ef3cbd80de35b64b
tigowler <aisapple1030@naver.com>
2022-02-28 16:56:48 +0900

    create user-setting page

--- src/main/java/com/neoAA/TinyBoard/controller/HomeController.java
+++ src/main/java/com/neoAA/TinyBoard/controller/HomeController.java
@@ -1,16 +1,33 @@
 package com.neoAA.TinyBoard.controller;
 
+import com.neoAA.TinyBoard.model.User;
+import com.neoAA.TinyBoard.repository.GuestRepository;
+import com.neoAA.TinyBoard.repository.PostRepository;
+import com.neoAA.TinyBoard.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Configuration
-@RequestMapping("/")
+@RequestMapping
 public class HomeController {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping
     public String index(){
         return "index";
     }
 
+    @GetMapping("/setting")
+    public String userSetting(Model model, Authentication authentication){
+        User user = userRepository.findByUsername(authentication.getName());
+        model.addAttribute("posts", user.getPost());
+        model.addAttribute("comments", user.getGuestBooks());
+        return "user-setting";
+    }
 
 }
